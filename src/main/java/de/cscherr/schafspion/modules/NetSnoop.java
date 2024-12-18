eddd7ee3f3244467fa6a90ecdecd01e3516b47b4
PlexSheep <software@cscherr.de>
2024-12-18 17:16:36 +0100

    feat(netsnoop): filter packets

--- src/main/java/de/cscherr/schafspion/modules/NetSnoop.java
+++ src/main/java/de/cscherr/schafspion/modules/NetSnoop.java
@@ -1,8 +1,8 @@
 package de.cscherr.schafspion.modules;
 
-import com.mojang.logging.LogUtils;
 import de.cscherr.schafspion.SchafSpion;
 import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.PacketListSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
@@ -12,6 +12,8 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Unique;
 
+import java.util.Set;
+
 /**
  * Adds the ability to capture network packets
  */
@@ -19,12 +21,18 @@ import org.spongepowered.asm.mixin.Unique;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class NetSnoop extends Module {
     @Unique
-    private static final Logger LOG = LogUtils.getLogger();
+    private static final Logger LOG = SchafSpion.LOG;
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
     public final Setting<Boolean> toFile = sgGeneral.add(new BoolSetting.Builder().name("log to file").description("log network packets to files in the .minecraft directory").defaultValue(false).build());
     public final Setting<Boolean> logRX = sgGeneral.add(new BoolSetting.Builder().name("log RX").description("log received").defaultValue(true).build());
     public final Setting<Boolean> logTX = sgGeneral.add(new BoolSetting.Builder().name("log TX").description("log transmitted").defaultValue(true).build());
     public final Setting<Verbosity> verbosity;
+    private final Setting<Set<Class<? extends Packet<?>>>> relevantPackets = sgGeneral.add(new PacketListSetting.Builder()
+        .name("Relevant packets")
+        .description("The packets to be logged")
+        .build()
+    );
+
 
     /**
      * Formats the packet description String
@@ -55,6 +63,11 @@ public class NetSnoop extends Module {
         switch (packet.getClass().getSimpleName()) {
             case "PositionAndOnGround" -> {
                 PlayerMoveC2SPacket pmPacket = (PlayerMoveC2SPacket)packet;
+                fieldInfo = String.format("X: %f | Y: %f | Z: %f\n" +
+                    "Ground: %b", pmPacket.getX(0), pmPacket.getY(0), pmPacket.getZ(0), pmPacket.isOnGround());
+            }
+            case "Full" -> {
+                PlayerMoveC2SPacket.Full pmPacket = (PlayerMoveC2SPacket.Full) packet;
                 fieldInfo = String.format("X: %f | Y: %f | Z: %f\n" +
                     "Ground: %b", pmPacket.getX(0), pmPacket.getY(0), pmPacket.getZ(0), pmPacket.isOnGround());
             }
@@ -90,8 +103,8 @@ public class NetSnoop extends Module {
      * @param packet the packet in question
      * @return True for those packets that should be ignored while logging, otherwise false
      */
-    public static boolean ignorePacket(Packet<?> packet) {
-        return false;
+    public boolean ignorePacket(Packet<?> packet) {
+        return !relevantPackets.get().contains(packet.getClass());
     }
 
     /** Logs a packet
@@ -112,7 +125,7 @@ public class NetSnoop extends Module {
             break;
         }
 
-        if (this.isActive() && !NetSnoop.ignorePacket(packet)) {
+        if (isActive() && !ignorePacket(packet)) {
             LOG.info(String.format("%s Package: %s", side, getPacketInfo(packet)));
         }
     }
